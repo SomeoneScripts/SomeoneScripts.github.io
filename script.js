1a70d131da4da2779fe4f2eb42bf86e557776495
SomeoneScripts <lucasviniciussoaresdasilva50@gmail.com>
2025-12-06 23:34:24 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const recent = [];
+const maxRecent = 4;
+
+function ChangeBackground() {
+  let index;
+  do {
+    index = Math.floor(Math.random() * 20);
+  } while (recent.includes(index));
+
+  recent.push(index);
+  if (recent.length > maxRecent) recent.shift();
+
+  document.body.style.backgroundImage = `url('https://raw.githubusercontent.com/SomeoneScripts/ReadIt/main/Images/Background${index}.png')`;
+}
+
+ChangeBackground();
+setInterval(ChangeBackground, 30000);
